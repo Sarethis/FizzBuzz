93c9612ca9df7e0c4bc73d016a0937ea50349cc2
Sareth <mateusz.kiela@gmail.com>
2022-02-20 21:42:41 +0100

    Add project files.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class FizzBuzz
+    {
+        private int _userValue;
+
+        public FizzBuzz (int userValue)
+        {
+            _userValue = userValue;
+        }
+
+        public void PlayTheGame()
+        {
+            if (_userValue % 3 == 0 && _userValue % 5 == 0)
+                Console.WriteLine("FizzBuzz!"); 
+            else if (_userValue%5 == 0)
+                Console.WriteLine("Buzz!");
+            else if (_userValue % 3 == 0)
+                Console.WriteLine("Fizz!");
+            else
+                Console.WriteLine($"To ani Fizz, ani Buzz: {_userValue}");
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        public static int GetValue()
+        {
+            while (true)
+            {
+                var userValue = Console.ReadLine();
+                if (userValue.ToUpper() == "E")
+                    Environment.Exit(0);
+                {
+                    if (!int.TryParse(userValue, out var value))
+                    {
+                        Console.WriteLine("Podana wartość nie jest liczbą!\nSpróbuj jeszcze raz: ");
+                        continue;
+                    }
+                    return value;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Cześć, tu gra FizzBuzz! Podaj liczbę!\nTwoja liczba: ");
+                var userValue = GetValue();
+
+                var fizzBuzz = new FizzBuzz(userValue);
+                fizzBuzz.PlayTheGame();
+                Console.WriteLine("Chcesz zagrać jeszcze raz? Jeśli nie wprowadz 'E'");
+            }
+        }
+    }
+}
